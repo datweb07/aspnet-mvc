05a79c81194d7bfae18880d984e5ff2e09a6d7e6
datweb07 <dat82770@gmail.com>
2026-04-28 14:19:27 +0700

    feat: implement aspnet mvc routing configuration and route mapping

---
+++ Controllers/HSKController.cs
@@ -0,0 +1,20 @@
+﻿using learn_asp.net_mvc.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace learn_asp.net_mvc.Controllers
+{
+    public class HSKController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("/p/{id}")]
+        [Route("/certificate/{id}")]
+        public IActionResult Certificate(string id)
+        {
+            return View(new HSK { id = id });
+        }
+    }
+}
---
+++ Models/Collection.cs
@@ -0,0 +1,7 @@
+﻿namespace learn_asp.net_mvc.Models
+{
+    public class Collection
+    {
+        public int Id { get; set; }
+    }
+}
